7bec15000dc044f5f2d3a4c778f3d452aa6547d6
Krishna Lagad <krishnalagad2@gmail.com>
2022-11-30 16:41:29 +0530

    Update HotelServiceImpl.java

--- hotel-service-hrs/src/main/java/com/hrs/hotel/services/impl/HotelServiceImpl.java
+++ hotel-service-hrs/src/main/java/com/hrs/hotel/services/impl/HotelServiceImpl.java
@@ -2,26 +2,33 @@ package com.hrs.hotel.services.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.hrs.hotel.entities.Hotel;
+import com.hrs.hotel.repositories.HotelRepository;
 import com.hrs.hotel.services.HotelService;
 
+@Service
 public class HotelServiceImpl implements HotelService {
+	
+	@Autowired
+	private HotelRepository hotelRepository;
 
 	@Override
 	public Hotel createHotel(Hotel hotel) {
-		// TODO Auto-generated method stub
+		this.hotelRepository.save(hotel);
 		return null;
 	}
 
 	@Override
 	public List<Hotel> getAllHotels() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Hotel> allHotels = this.hotelRepository.findAll();
+		return allHotels;
 	}
 
 	@Override
 	public Hotel getHotel(String hotelId) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
