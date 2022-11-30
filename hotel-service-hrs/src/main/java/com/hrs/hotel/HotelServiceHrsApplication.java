a1fe9d33c1838a746f278537d03f1507a2f9f83f
Krishna Lagad <krishnalagad2@gmail.com>
2022-11-30 16:26:58 +0530

    service 2 creation

---
+++ hotel-service-hrs/src/main/java/com/hrs/hotel/HotelServiceHrsApplication.java
@@ -0,0 +1,13 @@
+package com.hrs.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelServiceHrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelServiceHrsApplication.class, args);
+	}
+
+}
---
+++ hotel-service-hrs/src/test/java/com/hrs/hotel/HotelServiceHrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrs.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelServiceHrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
