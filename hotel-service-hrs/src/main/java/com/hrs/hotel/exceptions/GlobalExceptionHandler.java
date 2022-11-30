e57833d1a6dd9ef213c59444cf1eadcde0112f57
Krishna Lagad <krishnalagad2@gmail.com>
2022-11-30 16:58:39 +0530

    Exception handling

---
+++ hotel-service-hrs/src/main/java/com/hrs/hotel/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.hrs.hotel.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.hrs.hotel.payload.ApiResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<ApiResponse> handleResourceNotFoundException(ResourceNotFoundException ex) {
+		String message = ex.getMessage();
+		ApiResponse response = ApiResponse.builder().message(message).isSuccess(false).status(HttpStatus.NOT_FOUND)
+				.build();
+		
+		return new ResponseEntity<ApiResponse>(response, HttpStatus.NOT_FOUND);
+	}
+}
