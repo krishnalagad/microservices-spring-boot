c628ed3760b68ae6de318d858f35f683e4bf870b
Krishna Lagad <krishnalagad2@gmail.com>
2022-11-30 12:04:44 +0530

    service 1 creation
    - creating user service

---
+++ user-service-hrs/src/main/java/com/hrs/user/service/UserServiceHrsApplication.java
@@ -0,0 +1,13 @@
+package com.hrs.user.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceHrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceHrsApplication.class, args);
+	}
+
+}
