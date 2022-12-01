eaa6088c23882cef330637e7b20cd1c709300c7a
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-01 16:17:25 +0530

    service 3 creation

---
+++ rating-service-hrs/src/main/java/com/hrs/rating/RatingServiceHrsApplication.java
@@ -0,0 +1,13 @@
+package com.hrs.rating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingServiceHrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingServiceHrsApplication.class, args);
+	}
+
+}
---
+++ rating-service-hrs/src/test/java/com/hrs/rating/RatingServiceHrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrs.rating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingServiceHrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
